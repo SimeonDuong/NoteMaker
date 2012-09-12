e079124285ac099edd55b019195c345490b014bf
Simeon Duong <simeon.duong@gmail.com>
2012-09-12 01:26:29 -0500

    Initial commit, placeholder .rb file, ...features to come.

---
+++ notemaker.rb
@@ -0,0 +1,22 @@
+#!/bin/Ruby
+#notemaker.rb by Simeon Duong
+
+require 'date'
+
+
+#create new folder with todays date
+d = date.today
+d.strftime('%m%d%y')
+
+
+
+#check for most recent *.notes file
+
+#copy to-do if not marked with #
+
+#copy reminders if today < reminder date
+
+#copy previous notes as *.txt files into Drive folder
+
+
+
